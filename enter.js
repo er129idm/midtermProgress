605cc2af54169241af647621867d64d92ec11ae0
Jamsies99 <jamsies99@gmail.com>
2020-03-10 07:50:57 -0400

    Add files via upload

---
+++ enter.js
@@ -0,0 +1,12 @@
+function EnterFood(){
+  //change the picture
+    location.href = ('page2.html');
+  }
+
+function EnterDrink(){
+  location.href = ('page3.html');
+}
+
+function EnterHome(){
+  location.href = ('index.html');
+}
